254939ce031de798539fc5529ca80dae759f4ce2
Marko Laban <marko.laban@auckland.ac.nz>
2020-06-22 16:22:21 +1200

    WIP singularity

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM php:7.4-apache
 COPY src/ /var/www/html
 COPY php.ini $PHP_INI_DIR/conf.d/
-RUN apt-get update && apt-get install -y gnupg2
-RUN echo 'deb http://cloud.r-project.org/bin/linux/debian buster-cran40/' >> /etc/apt/sources.list
-RUN apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-RUN apt-get update
-RUN apt install -y -t buster-cran40 r-base r-base-dev r-recommended  libssl-dev libcurl4-openssl-dev libxml2-dev
-RUN mkdir /usr/share/man/man1/
-RUN apt-get install -y default-jre
-RUN apt-get install -y default-jdk
-RUN R CMD javareconf
-RUN Rscript /var/www/html/install_packages.R
+COPY main.sh main.sh
+# RUN apt-get update && apt-get install -y gnupg2
+# RUN echo 'deb http://cloud.r-project.org/bin/linux/debian buster-cran40/' >> /etc/apt/sources.list
+# RUN apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+# RUN apt-get update
+# RUN apt install -y -t buster-cran40 r-base r-base-dev r-recommended  libssl-dev libcurl4-openssl-dev libxml2-dev
+# RUN mkdir /usr/share/man/man1/
+# RUN apt-get install -y default-jre
+# RUN apt-get install -y default-jdk
+# RUN R CMD javareconf
+# RUN Rscript /var/www/html/install_packages.R
+# CMD ./main.sh
 RUN chmod +x /var/www/html/sws/csvtk
 EXPOSE 80
---
+++ main.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+echo 'Hello, World!'
+
+# dir                     <-  toString(args[1])
+# normalize               <-  toString(args[2])
+# email                   <-  toString(args[3])
+# snp                     <-  toString(args[4])
+# xy_chr                  <-  toString(args[5])
+# cr_pro                  <-  toString(args[6])
+# pheno                   <-  toString(args[7])
+# DM                      <-  toString(args[8])
+# data_load_method        <-  toString(args[9])           # 1 for champ and 2 for minfi
+# arraytype               <-  toString(args[10])          #"EPIC" and "450K"
+# deconv                  <-  toString(args[11])          
+# com_type                <-  toString(args[12])
+# array_corr              <-  toString(args[13])
+# batch_corr              <-  toString(args[14])
+# norm_name               <-  toString(args[15])
+
+MODE="console"
+EWAS_OUT = ""
+EWAS_UPLOADS = ""
+EWAS_RESULTS = ""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -m|--mode)
+    MODE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--out)
+    EWAS_OUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -u|--uploads)
+    EWAS_UPLOADS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--results)
+    EWAS_RESULTS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    echo "DEFAULT"
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+echo "MODE = $MODE"
+echo "EWAS_OUT = $EWAS_OUT"
+echo "EWAS_UPLOADS = $EWAS_UPLOADS"
+echo "EWAS_RESULTS = $EWAS_RESULTS"
+
+if [ "$MODE" = "web" ] 
+then
+    cd /var/www/html
+    apache2-foreground
+    # while true; do sleep 1000; done;
+fi
