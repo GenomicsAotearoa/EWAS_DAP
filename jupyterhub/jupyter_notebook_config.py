7f4a8ccfb9625c3c50325898c523edfa1339aaf1
Marko Laban <marko.laban@auckland.ac.nz>
2020-09-02 23:11:53 +0000

    Adding configuration and script files for jupyterhub

---
+++ jupyterhub/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+c.ServerProxy.servers = {
+  'EWASP': {
+    'command': ['/nesi/project/ga02964/run_ewas_arg', '{port}', '{base_url}EWASP'],
+    'timeout': 10,
+  },
+}
+
+# allow proxying to anywhere
+def host_whitelist(handler, host):
+    return True
+c.ServerProxy.host_whitelist = host_whitelist
